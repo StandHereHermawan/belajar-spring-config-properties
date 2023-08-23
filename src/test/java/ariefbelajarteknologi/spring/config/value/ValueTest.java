cf0a8a2f6a2ca3692e864ba8f9939b103b9884c1
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-23 10:06:20 +0700

    Value, ValueTest

---
+++ src/test/java/ariefbelajarteknologi/spring/config/value/ValueTest.java
@@ -0,0 +1,63 @@
+package ariefbelajarteknologi.spring.config.value;
+
+import lombok.Getter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Component;
+
+@SpringBootTest(classes = ValueTest.TestApplication.class)
+public class ValueTest {
+
+    @Autowired
+    private TestApplication.ApplicationProperties properties;
+
+    @Autowired
+    private TestApplication.SystemProperties systemProperties;
+
+    @Test
+    void testValue() {
+        Assertions.assertEquals("Belajar Spring Boot", properties.getName());
+        Assertions.assertEquals(1, properties.getVersion());
+        Assertions.assertEquals(false, properties.isProductionMode());
+    }
+
+    @Test
+    void testSystemProperties() {
+        // Assertions.assertEquals("D:\jdk-19.0.2", systemProperties.getJavaHome());
+        // ERROR gegara path windows lagi lol
+        System.out.println(systemProperties.getJavaHome());
+    }
+
+    @SpringBootApplication
+    public static class TestApplication {
+
+        @Component
+        @Getter
+        public static class SystemProperties {
+
+            @Value("${JAVA_HOME}")
+            private String javaHome;
+        }
+
+        @Component
+        @Getter
+        public static class ApplicationProperties {
+
+            @Value("${application.name}")
+            private String name;
+
+            @Value("${application.version}")
+            private Integer version;
+
+            @Value("${application.production-mode}")
+            private boolean productionMode;
+
+        }
+
+    }
+
+}
