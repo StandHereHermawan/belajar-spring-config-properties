72a4c6576005bb7f2d752acbd7447a86957c164d
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-23 09:32:33 +0700

    Environtment, EnvirontmentTest

---
+++ src/test/java/ariefbelajarteknologi/spring/config/environment/EnvironmentTest.java
@@ -0,0 +1,27 @@
+package ariefbelajarteknologi.spring.config.environment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
+
+@SpringBootTest
+public class EnvironmentTest {
+
+    @Autowired
+    private Environment environment;
+
+    @Test
+    void testEnvirontment() {
+        String javaHome = environment.getProperty("JAVA_HOME");
+        System.out.println(javaHome);
+        // Assertions.assertEquals("D:\jdk-19.0.2", javaHome);
+        // ERROR gegara path wondows lol
+    }
+
+    @SpringBootApplication
+    public static class TestApplication {
+
+    }
+}
