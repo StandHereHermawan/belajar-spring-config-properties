3f0724f1e11353c56017401e3fd6fb95674da447
StandHereHermawan <nolifegansaya@gmail.com>
2023-08-27 12:23:12 +0700

    Conversion, modify ConverterTest

--- src/test/java/ariefbelajarteknologi/spring/config/conversion/ConverterTest.java
+++ src/test/java/ariefbelajarteknologi/spring/config/conversion/ConverterTest.java
@@ -1,12 +1,17 @@
 package ariefbelajarteknologi.spring.config.conversion;
 
+import ariefbelajarteknologi.spring.config.converter.StringToDateConverter;
 import ariefbelajarteknologi.spring.config.properties.ApplicationProperties;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.convert.ApplicationConversionService;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.convert.ConversionService;
 
 import java.text.SimpleDateFormat;
 import java.time.Duration;
@@ -35,7 +40,16 @@ public class ConverterTest {
     @EnableConfigurationProperties({
             ApplicationProperties.class
     })
+    @Import(StringToDateConverter.class)
     public static class TestApplication {
 
+        @Bean
+        public ConversionService conversionService(StringToDateConverter stringToDateConverter) {
+            ApplicationConversionService conversionService = new ApplicationConversionService();
+            conversionService.addConverter(stringToDateConverter);
+            return conversionService;
+        }
+
     }
+
 }
